3a9655bff6eeeae40226f785530cb8df7b775093
Pratish Kumar Mohapatra <pratish.kumar@outlook.com>
2022-05-21 07:09:26 +0530

    Random commit-minimal changes and rectification

--- Inheritance02.java
+++ Inheritance02.java
@@ -6,6 +6,26 @@
 *Now create an object of 'Subclass' and call the abstract method and the non-abstract method. 
 */
 
+abstract class Abs{
+    Abs(){
+        System.out.println("This is constructor of abstract class");
+    }
+    public abstract void a_method();
+    public void normal() {
+        System.out.println("This is a normal method of abstract class");
+    }
+}
+class Sub_class extends Abs{
+    public void a_method() {
+        System.out.println("This is an abstract method");
+    }
+}
+
 public class Inheritance02 {
+    public static void main(String[] args) {
+    Sub_class s1obj = new Sub_class();
+    s1obj.a_method();
+    s1obj.normal();
+    }
     
 }
--- Tri.java
+++ Tri.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class Tri {
-  
+  /**Write Java methods to calculate the area of a triangle. 
+
+Input Side-1: 10                                                                               
+Input Side-2: 15                                                                               
+Input Side-3: 20    
+Expected Output:                                                                          
+The area of the triangle is 72.6184377413890 */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
