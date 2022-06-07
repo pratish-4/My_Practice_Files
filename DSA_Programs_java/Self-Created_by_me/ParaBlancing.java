574fd6bf3fe840ca807ec3f77e0932d0d691a4cf
Pratish Kumar Mohapatra <pratish.kumar@outlook.com>
2022-06-07 22:45:18 +0530

    Commit

--- DSA_Programs_java/Self-Created_by_me/ParaBlancing.java
+++ DSA_Programs_java/Self-Created_by_me/ParaBlancing.java
@@ -3,38 +3,31 @@ import java.util.*;
 public class ParaBlancing {
     public static boolean match(String parens) {
         //write your code here
-        int len = parens.length();
-        if(len % 2 !=0){
+        Stack <Character> sc = new Stack<>();
+        if(parens.length() %2 !=0){
             return false;
         }
         else{
-            Stack <Character> stackChar = new Stack<>();
-            for (int i = 0; i <len ; i++){
+            for(int i = 0; i <parens.length();i++){
                 char data = parens.charAt(i);
-                if(data != '(' || data !=')'){
-                    return false;
+                if(data == '('){
+                sc.push(data);
                 }
                 else{
-                if (data == '('){
-                    stackChar.push(data);
-                }
-                else if(data == ')' && !stackChar.empty()){
-                    char ck = stackChar.pop();
-                    // check 
-                    if(ck == '(' && data != ')'){
+                    if(!sc.isEmpty()){
+                       char ch = sc.pop();
+                        if(ch !='(' && data == ')'){
+                            return false;
+                        }
+                    }
+                    else{
                         return false;
                     }
-                    }
                 }
-                }
-                return true;
             }
-            
         }
-
-    
-                
-
+        return true;
+              }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println(match(in.nextLine()));
