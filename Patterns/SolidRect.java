7bd9b1627b706ffbd8b8e0e150156a83de145293
Pratish Kumar Mohapatra <pratish.kumar@outlook.com>
2022-07-06 07:30:22 +0530

    Add files via upload

---
+++ Patterns/SolidRect.java
@@ -0,0 +1,15 @@
+/**
+ * SolidRect
+ */
+public class SolidRect {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
