c20e485efd78013741122fee718b3f45fd115fe0
Pratish Kumar Mohapatra <pratish.kumar@outlook.com>
2022-07-06 12:22:24 +0530

    Add files via upload

---
+++ Patterns/FloydsTri.java
@@ -0,0 +1,17 @@
+/**
+ * FloydsTri
+ */
+public class FloydsTri {
+    public static void main(String[] args) {
+        int n = 5;
+        int number = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(number+" ");
+                number++;
+            }
+    
+            System.out.println();
+        }
+    }
+}
